ea484e6a8b90b089faab9a3ce0d4902ba384e396
Saki Kimiduka <s.kimizuka@hotmail.co.jp>
2020-11-27 00:08:10 +0900

    create functional component.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@
 function App() {
   return (
     <div>
+      <Cat />
       <label htmlFor="bar">bar</label>
       <input type="text" onChange={() => {console.log("I'm React!")}} />
     </div>
   );
+}
+
+function Cat() {
+  return <div>Maiow!</div>
 }
 
 export default App;
